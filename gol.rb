ae4750d70969925a621f734c74f60d1b76143753
Alexander Lay-Calvert <alex.laycalvert@gmail.com>
2022-10-20 11:26:09 -0400

    Updated formula

--- gol.rb
+++ gol.rb
@@ -2,8 +2,7 @@ class Gol < Formula
   desc "Implementation of John Conway's Game of Life in C++"
   homepage "https://github.com/alex-laycalvert/gol"
   url "https://github.com/alex-laycalvert/gol/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "5aa0f70e693715eff063ffc30605d1f08e0f895d25632fe69b52fef1a9
-8bda11"
+  sha256 "81ac7e9204ac50d98ec7479854d13cfde47fdc60ee4cc933f6c9f3c1879fdd47"
   license "MIT"
 
   uses_from_macos "ncurses"
@@ -11,8 +10,7 @@ class Gol < Formula
   def install
     prefix.install "README.md"
     prefix.install "LICENSE"
-    system "make"
-    prefix.install "build/gol"
+    system "make install"
   end
 
   test do
