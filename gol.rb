86ed121c02faa17b58dc39e61fa5f0b058bad329
Alexander Lay-Calvert <alex.laycalvert@gmail.com>
2022-10-20 11:29:52 -0400

    Updated formula

--- gol.rb
+++ gol.rb
@@ -1,9 +1,11 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Gol < Formula
   desc "Implementation of John Conway's Game of Life in C++"
   homepage "https://github.com/alex-laycalvert/gol"
-  url "https://github.com/alex-laycalvert/gol/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "81ac7e9204ac50d98ec7479854d13cfde47fdc60ee4cc933f6c9f3c1879fdd47"
   license "MIT"
+  head "https://github.com/alex-laycalvert/gol"
 
   uses_from_macos "ncurses"
 
